723a51bb892c1ffe40a4073eefab41c086703d29
Ricardo Francisco <ri22sp@gmail.com>
2026-01-13 23:28:15 -0300

    Removidas as redundâncias:

---
+++ api/postgres-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e
+
+echo "🔐 Carregando secrets do Postgres"
+
+if [ -f /run/secrets/postgres.env ]; then
+  while IFS= read -r line || [ -n "$line" ]; do
+    case "$line" in
+      ""|\#*) continue ;;
+    esac
+    export "$line"
+  done < /run/secrets/postgres.env
+fi
+
+echo "🚀 Iniciando Postgres"
+exec docker-entrypoint.sh postgres \
+  -c max_connections=200 \
+  -c shared_buffers=256MB \
+  -c effective_cache_size=1GB \
+  -c maintenance_work_mem=64MB \
+  -c checkpoint_completion_target=0.9 \
+  -c wal_buffers=16MB \
+  -c default_statistics_target=100 \
+  -c random_page_cost=1.1 \
+  -c effective_io_concurrency=200 \
+  -c work_mem=4MB \
+  -c min_wal_size=1GB \
+  -c max_wal_size=4GB \
+  -c max_worker_processes=4 \
+  -c max_parallel_workers_per_gather=2 \
+  -c max_parallel_workers=4 \
+  -c max_parallel_maintenance_workers=2
